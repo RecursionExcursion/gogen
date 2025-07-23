13cb6e54a96ae6ea7541efd3a2240b933407a2bb
RecursionExcursion <rloup15@gmail.com>
2025-07-23 01:09:57 -0400

    rework code gen to use templates

--- internal/code_generation.go
+++ internal/code_generation.go
@@ -1,75 +1,49 @@
 package internal
 
 import (
-	"fmt"
+	"bytes"
 	"os"
-	"strings"
+	"text/template"
 )
-
-type script struct {
-	code string
-}
-
-func (s *script) addLine(l string) {
-	s.code += "\n" + l
-}
 
 // GenerateScript - creates a go script that opens files/urls.
 // f- file where the script is going to be created.
 // args- urls/paths to open.
 func GenerateScript(f *os.File, args ...string) error {
-	//Create base script and imports
-	fileContent := script{
-		code: genPackageStatement(),
+
+	var buff bytes.Buffer
+
+	//write exeFn declaration
+	buff.WriteString(execFnTemplate + "\n")
+
+	//parse other templates
+	tmp, err := template.New("main").Parse(mainFuncTemplate)
+	if err != nil {
+		return err
+	}
+	tmp.New("exeFnCall").Parse(execFnCallTemplate)
+
+	//inject args into exeFn calls
+	var exeFnCallBuff bytes.Buffer
+
+	for _, arg := range args {
+		tmp.ExecuteTemplate(&exeFnCallBuff, "exeFnCall", struct {
+			Arg string
+		}{
+			arg,
+		})
+		exeFnCallBuff.WriteString("\n")
 	}
 
-	fileContent.addLine(createImportStatement(execFnTemplate.imports...))
+	tmp.ExecuteTemplate(&buff, "main", struct {
+		Arg string
+	}{
+		exeFnCallBuff.String(),
+	})
 
-	//create main fn logic
-	mainFnScript := script{
-		code: "",
-	}
-
-	//add exefn calls with args
-	for _, a := range args {
-
-		execCall := execFnCallTemplate
-		execCall.inject(fmt.Sprintf("\"%v\"", a))
-		mainFnScript.addLine(execCall.code)
-	}
-
-	//inject code into mainfn wrapper
-	mainFn := mainFuncTemplate
-	mainFn.inject(mainFnScript.code)
-
-	//add main fn and logic to file
-	fileContent.addLine(mainFn.code)
-	//Add exefn at bottom of file
-	fileContent.addLine(execFnTemplate.code)
-
-	_, err := f.Write([]byte(fileContent.code))
+	_, err = f.Write(buff.Bytes())
 	if err != nil {
 		return err
 	}
 	return nil
-}
-
-func genPackageStatement() string {
-	return "package main"
-}
-
-func createImportStatement(args ...string) string {
-	switch len(args) {
-	case 0:
-		return ""
-	case 1:
-		return fmt.Sprintf("import \"%v\"", args[0])
-	default:
-		str := "import (<i>)"
-		importStr := ""
-		for _, a := range args {
-			importStr += fmt.Sprintf("\"%v\"\n", a)
-		}
-		return strings.Replace(str, "<i>", importStr, 1)
-	}
 }
--- internal/code_templates.go
+++ internal/code_templates.go
@@ -1,54 +1,38 @@
 package internal
 
+var mainFuncTemplate = `
+func main(){
+	{{.Arg}}
+}`
+
+var execFnCallTemplate = `execCommand({{.Arg}})`
+
+var execFnTemplate = `
+package main
+
 import (
+	"fmt"
+	"os/exec"
+	"runtime"
 	"strings"
+	"log"
 )
-
-type codeTemplate struct {
-	imports    []string
-	code       string
-	delimmiter string
-}
-
-func (ct *codeTemplate) inject(in string) {
-	ct.code = strings.Replace(ct.code, ct.delimmiter, in, 1)
-}
-
-var mainFuncTemplate = codeTemplate{
-	code: `func main(){
-		<args>
-	}`,
-	delimmiter: "<args>",
-}
-
-var execFnCallTemplate = codeTemplate{
-	code:       `execCommand(<args>)`,
-	delimmiter: `<args>`,
-}
-
-var execFnTemplate = codeTemplate{
-	imports: []string{
-		"fmt",
-		"os/exec",
-		"runtime",
-		"strings",
-		"log",
-	},
-	code: `func execCommand(path string) error {
+	
+	func execCommand(arg string) error {
 
 	cmdPrefix := "cmd:"
 	urlPrefix := "url:"
 
 	var cmd *exec.Cmd
-	if strings.HasPrefix(path, cmdPrefix) {
-		parts := strings.SplitN(path, ":", 2)
+	if strings.HasPrefix(arg, cmdPrefix) {
+		parts := strings.SplitN(aeg, ":", 2)
 		if len(parts) != 2 {
 			log.Fatal("invalid code command")
 		}
 		cmdParts := strings.SplitN(parts[1], " ", -1)
 		cmd = exec.Command(cmdParts[0], cmdParts[1:]...)
-	} else if strings.HasPrefix(path, urlPrefix) {
-		parts := strings.SplitN(path, ":", 2)
+	} else if strings.HasPrefix(arg, urlPrefix) {
+		parts := strings.SplitN(arg, ":", 2)
 		if len(parts) != 2 {
 			log.Fatal("invalid code command")
 		}
@@ -69,5 +53,4 @@ var execFnTemplate = codeTemplate{
 	}
 
 	return cmd.Start()
-}`,
-}
+}`
